f8947cac1561118025a8dc71ae7cc426c014ddbb
Tema-ilit <artem_eliseev_2015@mail.ru>
2024-05-21 23:54:05 +0300

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,8 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  publicDir: `/Rick-and-Morty-/`,
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
